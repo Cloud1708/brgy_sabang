777af8fe75154ba31ef03c757d29a52257c388dd
Cloud08 <ch512291@gmail.com>
2025-10-09 16:37:29 +0800

    updated credentials

--- inc/db.php
+++ inc/db.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection (adjust credentials)
-$DB_HOST = '127.0.0.1';
-$DB_USER = 'root';
-$DB_PASS = '';
-$DB_NAME = 'qwerty';
+$DB_HOST = 'mysql.hostinger.com';
+$DB_USER = 'u689218423_brgysabang';
+$DB_PASS = '@Brgy_sabang12345';
+$DB_NAME = 'u689218423_brgysabang';
 
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 if ($mysqli->connect_error) {
