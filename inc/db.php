d5d7492304f34f0cba4334e9c2a55aaa2c1e0f95
Cloud1708 <ch512291@gnail.com>
2025-10-14 20:11:09 +0800

    push

--- inc/db.php
+++ inc/db.php
@@ -1,10 +1,10 @@
 <?php
 date_default_timezone_set('Asia/Manila');
 // Database connection (adjust credentials)
-$DB_HOST = 'localhost';
-$DB_USER = 'root';
-$DB_PASS = '';
-$DB_NAME = 'qwerty';
+$DB_HOST = 'mysql.hostinger.com';
+$DB_USER = 'u689218423_brgysabang';
+$DB_PASS = '@Brgy_sabang12345';
+$DB_NAME = 'u689218423_brgysabang';
 
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 if ($mysqli->connect_error) {
