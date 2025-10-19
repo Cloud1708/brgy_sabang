acf43a6c5eaf32fc262e1b3902d09f820c87845f
Cloud08 <ch512291@gmail.com  git config --global user.name>
2025-10-20 00:13:53 +0800

    pusing

--- inc/db.php
+++ inc/db.php
@@ -5,6 +5,7 @@ $DB_HOST = 'mysql.hostinger.com';
 $DB_USER = 'u689218423_brgysabang';
 $DB_PASS = '@Brgy_sabang12345';
 $DB_NAME = 'u689218423_brgysabang';
+
  
 
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
