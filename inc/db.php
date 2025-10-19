b273c3e6140892ec99e24000dda87b67821d32a9
Cloud08 <ch512291@gmail.com  git config --global user.name>
2025-10-20 00:41:21 +0800

    push

--- inc/db.php
+++ inc/db.php
@@ -6,8 +6,6 @@ $DB_USER = 'u689218423_brgysabang';
 $DB_PASS = '@Brgy_sabang12345';
 $DB_NAME = 'u689218423_brgysabang';
 
- 
-
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 if ($mysqli->connect_error) {
     die('Database connection failed: ' . $mysqli->connect_error);
